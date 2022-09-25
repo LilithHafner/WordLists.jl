77a1e43b2b55ad51b1ef514030fd089aa4725150
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-09-25 07:58:23 +0600

    add documentation

--- README.md
+++ README.md
@@ -4,3 +4,28 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://LilithHafner.github.io/WordLists.jl/dev/)
 [![Build Status](https://github.com/LilithHafner/WordLists.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/LilithHafner/WordLists.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/LilithHafner/WordLists.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/LilithHafner/WordLists.jl)
+
+WordLists.jl provides lists of words from various languages (currently English and Spanish).
+
+```julia-repl
+julia> words("English")
+257874-element Vector{String}:
+ "Aani"
+ "Aaron"
+ "Aaronic"
+ "Aaronical"
+ "Aaronite"
+ "Aaronitic"
+ ⋮
+ "zymotize"
+ "zymotoxic"
+ "zymurgy"
+ "zythem"
+ "zythum"
+```
+
+## Help Wanted
+
+The current set of languages is small and the list of Spanish words is incomplete. If you
+have access to a higher quality list of spanish words or words from a different language,
+please open a pull request or issue.
--- src/WordLists.jl
+++ src/WordLists.jl
@@ -1,7 +1,73 @@
+"""
+A module containing the function `words`.
+
+```julia-repl
+julia> words("english")
+257874-element Vector{String}:
+ "Aani"
+ "Aaron"
+ "Aaronic"
+ ⋮
+ "zymurgy"
+ "zythem"
+ "zythum"
+```
+"""
 module WordLists
 
 export words
 
+"""
+    words(languages...; all)
+
+List the words in a language as a sorted `Vector{String}`.
+
+The lists are incomplete and their exact contents are subject to change. Setting `all=true`
+will make an effort to include all words, but at the cost of including some non-words.
+
+Languages can be queried by iso code, name, or english name and are not case sensitive.
+
+The following languages are currently supported:
+
+- English: "english", "eng", "en"
+- Spanish: "spanish", "español", "spa", "es"
+
+Multilingual lists are supported. For example, `words("english", "spanish")` will return a
+sorted list containing words from both English and Spanish.
+
+# Examples
+
+```julia-repl
+julia> words("english")
+257874-element Vector{String}:
+ "Aani"
+ "Aaron"
+ "Aaronic"
+ ⋮
+ "zymurgy"
+ "zythem"
+ "zythum"
+
+julia> words("es", "en")
+257874-element Vector{String}:
+ "Aani"
+ "Aaron"
+ "Aaronic"
+ ⋮
+ "útero"
+ "útil"
+ "útiles"
+
+julia> rand(words("EnGLiSH", "EspañoL"; all=true), 6)
+6-element Vector{String}:
+ "Upham"
+ "asentir"
+ "butcher"
+ "fireworm"
+ "cirrolite"
+ "revocable"
+```
+"""
 function words(languages::AbstractString...; all=false)
     lists = Vector{String}[]
     content = joinpath(dirname(@__DIR__), "lists")
